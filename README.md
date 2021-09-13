416e012eb3b1e50d6549a7d50ad81ff0c6e14c25
Matías Gutiérrez <matyuh@hotmail.com>
2021-09-13 12:18:41 -0300

    connected abi + address of deployed contract to react frontend

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lottery Contract
+
+With the lottery_eth_solidity (check the repo) contract deployed in Rinkeby Eth Network, made a React frontend to interact with it.
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { manager: ''};
+  }
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    this.setState({ manager });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Lottery Contract!</h1>
+        <p>This contract is managed by {this.state.manager}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
