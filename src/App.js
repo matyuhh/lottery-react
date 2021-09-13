463d14fb78d52370877abd9f2a642429b02fe8a4
Matías Gutiérrez <matyuh@hotmail.com>
2021-09-13 18:04:06 -0300

    added pick a winner button + onclick function

--- src/App.js
+++ src/App.js
@@ -34,6 +34,18 @@ class App extends Component {
 
     this.setState({message: 'You have been entered!'});
   };
+  
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({message: 'Waiting on transaction success...'});
+    
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+    
+    this.setState({ message: 'A winner has been picked!'});
+  }
 
   render() {
     return (
@@ -54,11 +66,14 @@ class App extends Component {
               />
             </div>
             <button>Enter</button>
+          </form> 
 
-            <hr/>
+          <hr/>
+          <h4>Ready to pick a winner?</h4>
+          <button onClick={this.onClick}>Pick a winner!</button>
+          <hr/>
+          <h2>{this.state.message}</h2> 
 
-            <h2>{this.state.message}</h2>
-          </form>     
         </div>
       </div>
     );
